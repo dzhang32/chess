045372cda8a8e9bbaf3127897b304e46b4d44508
David Zhang <dyzhang32@gmail.com>
2021-02-24 18:30:36 +0000

    add docstring based documentation

--- chess_api.py
+++ chess_api.py
@@ -9,12 +9,28 @@ Created on Mon Feb 22 23:01:08 2021
 import requests
 
 def get_json(url):
+    """Retrieves JSON data from URL in a list format
+
+    @type url: str
+    @param url: URL of interest. 
+    @rtype: list
+    @returns: a list containing the queried JSON data.
+    """
     
     url_json = requests.get(url).json()
     
     return url_json
 
-def get_player(user, type = "profile"):
+def get_player(user, which = "profile"):
+    """Obtains the chess.com data for a particular player
+
+    @type user: user
+    @param user: username of the player of interest. 
+    @type which: str
+    @param which: which kind of data to to be downloaded.
+    @rtype: list
+    @returns: a list containing the chess.com players info. 
+    """
     
     if type == "profile": 
         
