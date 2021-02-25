ceb56acd6fa6225a80bd020fe2afa2a4ef926669
David Zhang <dyzhang32@gmail.com>
2021-02-25 21:17:19 +0000

    Add code to download other countries

--- scripts/01a-num_new_players.py
+++ scripts/01a-num_new_players.py
@@ -11,8 +11,9 @@ import pandas as pd
 import datetime as dt
 import os
 from tqdm import tqdm
+import random
 
-def main(iso, info, rel_results_dir):
+def main(iso, info, n, rel_results_dir):
     
     # use a relative path to the script
     # to determine output path, to allow transferability between machines
@@ -25,14 +26,13 @@ def main(iso, info, rel_results_dir):
     
     print("Downloading %s players '%s' info to %s" % (iso, info, results_dir))
     
-    players_info = get_country_players_info(iso, info)
+    players_info = get_country_players_info(iso, info, n)
     
     players_info.to_csv(results_dir + "/" + iso + "_players_" + info + "_" + 
                         dt.date.today().strftime("%d_%m_%Y") + ".csv", 
                         index = False)
-    
 
-def get_country_players_info(iso, info):
+def get_country_players_info(iso, info, n):
     """Retrieves one aspect of player profiles for all players in a country
     
     @type iso: str
@@ -48,7 +48,10 @@ def get_country_players_info(iso, info):
 
     players_info = []
     
-    for i in tqdm(range(len(players))):
+    # select index of random n players
+    player_indexes = random.sample(range(0, len(players)), n)
+    
+    for i in tqdm(player_indexes):
         
         player = cdc.caller.get_player_profile(players[i]).json
         players_info.append(player[info])
@@ -59,4 +62,7 @@ def get_country_players_info(iso, info):
 
 if __name__ == "__main__":
     
-    main(iso = "GB", info = "joined", rel_results_dir = "../data")
+    main(iso = "GB", info = "joined", n = 1000, rel_results_dir = "../data")
+    main(iso = "IT", info = "joined", n = 1000, rel_results_dir = "../data")
+    main(iso = "CO", info = "joined", n = 1000, rel_results_dir = "../data")
+    main(iso = "CN", info = "joined", n = 1000, rel_results_dir = "../data")
